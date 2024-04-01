58e0bd560b87f52a72f7769ea76920207c274b48
anasouardini <ouardinianas@proton.me>
2024-04-01 00:49:09 +0000

    refactor: made types more verbose and flexible

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,17 @@
-export const stagesMap = {
-    f: 'fresh',
-    v: 'viewed',
-    m: 'memorized',
-    r: 'revision',
-} as const;
+import { TranslationsTable, type WordsTable } from './model/dbTypes';
 
-export type Stage = (typeof stagesMap)[keyof typeof stagesMap];
+export type Stage = 'fresh' | 'viewed' | 'memorized' | 'revision';
+
+export type Lang = 'english' | 'french' | 'spanish';
+export type LangShort = 'en' | 'fr' | 'es';
+export const languagesMap: Record<LangShort, Lang> = {
+  en: 'english',
+  fr: 'french',
+  es: 'spanish',
+};
+
+export type WordType = "pronoun" | "noun" | "verb" | "adjective" | "adverb" | "preposition" | "conjunction" | "interjection" | "article" | "determiner" | "numeral" | "preposition" | "conjunction" | "interjection" | "article" | "determiner" | "numeral";
+
+export interface TranslationOutput extends WordsTable {
+  translations: TranslationsTable["translations"];
+}
