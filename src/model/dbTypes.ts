5ec55f6afe1fc5fcbedb315f22cc4fdd8bafd9bf
anasouardini <ouardinianas@proton.me>
2024-04-01 00:50:09 +0000

    refactor: more verbosity to the types

--- src/model/dbTypes.ts
+++ src/model/dbTypes.ts
@@ -1,39 +1,33 @@
+import { WordType, type Stage } from '../types';
+
 export interface WordsTable {
   word: string;
   lang: string;
+  frequencyOrder: number | null;
   audioLink: string | null;
-  frequency: number | null;
-  stage: 'fresh' | 'viewed' | 'memorized' | 'revision';
-  types:
-  (
-    | 'N' // Noun
-    | 'V' // Verb
-    | 'ADJ' // Adjective
-    | 'ADV' // Adverb
-    | 'PRON' // Pronoun
-    | 'PREP' // Preposition
-    | 'CONJ' // Conjunction
-    | 'ART' // Article
-    | 'DET' // Determiner
-    | 'INTJ' // Interjection
-    | 'NUM' // Numeral
-    | 'PART' // Particle
-    | 'AUX' // Auxiliary
-    | 'MOD' // Modal
-    | 'SCONJ' // Subordinating Conjunction
-    | 'CCONJ' // Coordinating Conjunction
-    | 'SYM' // Symbol
-  )[];
+  stage: Stage;
+  types: WordType[];
   synonyms: string[];
   definitions: string[];
   examples: string[];
+}
+type WordsRequiedProps = Omit<
+  WordsTable,
+  'audioLink' | 'types' | 'synonyms' | 'definitions' | 'examples'
+>;
+export interface WordsCreate extends WordsRequiedProps {
+  audioLink?: string | null;
+  types?: WordType[];
+  synonyms?: string[];
+  definitions?: string[];
+  examples?: string[];
 }
 
 export interface TranslationsTable {
   word: string;
   srcLang: string;
   destLang: string;
-  translations: string[];
+  translations: { type: WordType, translations: string[] }[];
 }
 
 export interface WordLogsTable {
