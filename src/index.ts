c787989847ff976798b7f9a8aeaa694a9a8f79e4
anasouardini <ouardinianas@proton.me>
2024-04-01 00:48:09 +0000

    feat: understanding this dictioanry API (WIP)

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
-import fs, { promises as fsp } from 'fs';
-import path from 'path';
-import db from './model/db';
-import schema from './model/schema';
 import controllers from './controller';
 import { type Stage } from './types';
+import lookup from './tools/lookupTranslation';
+import path from 'path';
+import populate from './tools/populateWords';
+import schema from './model/schema';
 
-// schema.update();
-// process.exit(0);
+(async () => {
+  // ------------------- schema
+  // schema.update();
+  // process.exit(0);
 
-const args = process.argv.slice(2);
-type Mode = 'consume';
-const mode: Mode = args[0] as Mode;
-if (mode !== 'consume') {
+  // ------------------- populate new words
+  // populate({ path: path.join('src', 'data', 'en', 'top-5k.txt'), lang: 'en' });
+  // populate({ path: path.join('src', 'data', 'fr', 'top-5k.txt'), lang: 'fr' });
+  // process.exit(0);
+
+  // ------------------- lookup new words' translations
+  const translations = await lookup({ word: 'bon', srcLang: 'fr', destLang: 'en' });
+  console.log(translations);
   process.exit(0);
-}
 
-// dispatcher
-controllers[mode](args.slice(1));
+  /*
+  const args = process.argv.slice(2);
+  type Mode = 'consume';
+  const mode: Mode = args[0] as Mode;
+  if (mode !== 'consume') {
+    process.exit(0);
+  }
+  
+  // dispatcher
+  controllers[mode](args.slice(1));
+  */
+})()
