d4e6a78c7fc21ba704630b0936f636ef862e561b
anasouardini <ouardinianas@proton.me>
2024-04-01 00:50:33 +0000

    feat: add read functionality to translation model

--- src/model/translations.ts
+++ src/model/translations.ts
@@ -1,10 +1,30 @@
 import db from './db';
+import { type Lang, LangShort, TranslationOutput } from '../types';
 
-export async function read({ word, lang }: { word: string; lang: string }) {
-  return await db
-    .selectFrom('translations')
-    .selectAll()
-    .where('word', '=', word)
-    .where('srcLang', '=', lang)
-    .executeTakeFirstOrThrow();
+interface ReadParams {
+  stage: string;
+  srcLang: LangShort;
+  destLang: LangShort;
 }
+function read({
+  stage,
+  srcLang,
+  destLang,
+}: ReadParams): TranslationOutput[] | undefined {
+  const statement = db.prepare(`
+    select T.translations, W.* from translations T inner join words W
+      on T.word = W.word and T.srcLang = W.lang
+      where W.stage = $stage and T.srcLang = $srcLang
+      and T.destLang = $destLang
+  `);
+
+  const resp = statement.all({ stage, srcLang, destLang });
+  if (!resp) {
+    console.log(`Err -> stage '${stage}' or lang '${srcLang}' doesn't exist`);
+    return undefined;
+  }
+
+  return resp as TranslationOutput[];
+}
+
+export default { read };
